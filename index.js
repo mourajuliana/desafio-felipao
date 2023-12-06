991bc419b54c09413b874f5533a7aaf2ec703b86
mourajuliana <mourajuliana@gmail.com>
2023-12-06 15:24:34 -0300

    segundo

---
+++ index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Pokemon";
+let xpHeroi = 7.001;
+
+if (xpHeroi <= 1.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Ferro.`)
+}
+else if (xpHeroi > 1.000 && xpHeroi <= 2.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Bronze.`)
+}
+else if (xpHeroi > 2.000 && xpHeroi <= 5.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Prata.`)
+}
+else if (xpHeroi > 5.000 && xpHeroi <= 7.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Ouro.`)
+}
+else if (xpHeroi >7.000 && xpHeroi <= 8.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Platina.`)
+}
+else if (xpHeroi > 8.000 && xpHeroi <= 9.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Ascendente.`)
+}
+else if (xpHeroi > 9.000 && xpHeroi <= 10.000) {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Imortal.`)
+}
+else {
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de Radiante.`)
+}
